f3b79c4d72330a3e151fcfc4d1af62c2261ad838
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-11 04:18:08 -0500

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Python Packages not yet in Nixpkgs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+
+    let
+
+      system = "x86_64-linux";
+
+      overlay = import ./default.nix;
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ overlay ];
+        config.allowUnfree = true;
+      };
+
+    in
+
+  {
+
+    packages.${system} = pkgs;
+
+  };
+}
