e1ed205b21ae91f3cc0d61a9b639a0ba19244941
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-22 09:49:11 -0500

    Is it working now?

--- default.nix
+++ default.nix
@@ -1,19 +1,31 @@
-self: super:
+final: prev:
 
-  rec {
+  with prev; {
 
-    callable-module         = self.callPackage ./callable-module.nix { };
-    is-instance             = self.callPackage ./is-instance.nix { inherit (self) callable-module; };
-    assure                  = self.callPackage ./assure.nix { };
-    mmry                    = self.callPackage ./mmry.nix { };
-    embd                    = self.callPackage ./embd.nix { inherit (self) is-instance assure mmry; };
-    kern                    = self.callPackage ./kern.nix { inherit (self) assure mmry; };
-    wnix                    = self.callPackage ./wnix.nix { inherit (self) is-instance assure mmry embd kern; };
-    python-bin              = self.callPackage ./python-bin.nix { inherit (self) python-cowsay; };
+    pythonPackagesExtensions = [
 
-    dvc-s3                  = self.callPackage ./dvc-s3.nix { };
-    lightgbm                = self.callPackage ./lightgbm.nix { };
-    python-cowsay           = self.callPackage ./python-cowsay.nix { };
-    tflite-runtime          = self.callPackage ./tflite-runtime.nix { };
-    lambda-multiprocessing  = self.callPackage ./lambda-multiprocessing.nix { };
+      (self: super:
+
+        rec {
+
+          callable-module         = self.callPackage ./callable-module.nix { };
+          is-instance             = self.callPackage ./is-instance.nix { inherit (self) callable-module; };
+          assure                  = self.callPackage ./assure.nix { };
+          mmry                    = self.callPackage ./mmry.nix { };
+          embd                    = self.callPackage ./embd.nix { inherit (self) is-instance assure mmry; };
+          kern                    = self.callPackage ./kern.nix { inherit (self) assure mmry; };
+          wnix                    = self.callPackage ./wnix.nix { inherit (self) is-instance assure mmry embd kern; };
+          python-bin              = self.callPackage ./python-bin.nix { inherit (self) python-cowsay; };
+
+          dvc-s3                  = self.callPackage ./dvc-s3.nix { };
+          lightgbm                = self.callPackage ./lightgbm.nix { };
+          python-cowsay           = self.callPackage ./python-cowsay.nix { };
+          tflite-runtime          = self.callPackage ./tflite-runtime.nix { };
+          lambda-multiprocessing  = self.callPackage ./lambda-multiprocessing.nix { };
+        }
+
+      )
+
+    ];
+
   }
--- flake.nix
+++ flake.nix
@@ -11,9 +11,7 @@
 
       system = "x86_64-linux";
 
-      overlay = (final: prev: with prev; rec {
-        pythonPackagesExtensions = [ (import ./default.nix prev ) ];
-      });
+      overlay = (import ./default.nix);
 
       pkgs = import nixpkgs {
         inherit system;
